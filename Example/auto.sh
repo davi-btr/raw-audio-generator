d77687991e7b5784beb7c1d3f992c2f1faa13fc4
davi-btr <105121115+davi-btr@users.noreply.github.com>
2022-07-10 19:46:31 +0200

    Update auto.sh

--- Example/auto.sh
+++ Example/auto.sh
@@ -1,7 +1,7 @@
 
 for n in $(seq 64 $[64 + 12])
 do
-        ./rawdata -S /usr/share/sounds/sf2/FluidR3_GM.sf2 -r 48000.0 -b 1024 -f float -t float -N 1 0 $n 127
+        ./rawdata -S /usr/share/sounds/sf2/FluidR3_GM.sf2 -r 48000.0 -b 1024 -f float -t float -N 2 0 $n 127 3 0 $n 0
 done
 
-./rawdata -S /usr/share/sounds/sf2/FluidR3_GM.sf2 -r 48000.0 -b 1024 -f float -t float -N 2 0 64 127 0 75 127 
+./rawdata -S /usr/share/sounds/sf2/FluidR3_GM.sf2 -r 48000.0 -b 1024 -f float -t float -N 2 0 64 127 0 0 75 127 3 0 64 0 0 0 75 0 
